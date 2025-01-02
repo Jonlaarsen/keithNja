27ed0736c377a712f39ec872271b1d670670a6c6
Jonlaarsen <jontoftdallarsen@icloud.com>
2025-01-02 16:03:51 +0900

    test

---
+++ app/api/create.ts
@@ -0,0 +1,28 @@
+import { neon } from '@neondatabase/serverless';
+
+export const create = async (formData: FormData) => {
+  'use server';
+
+  const sql = neon(`${process.env.DATABASE_URL}`);
+  const title = formData.get('title') as string;
+  const subtitle = formData.get('subtitle') as string | null;
+  const description = formData.get('description') as string | null;
+  const imgURL = formData.get('imgURL') as string;
+  const videoURL = formData.get('videoURL') as string;
+
+  if (!title || !imgURL || !videoURL) {
+    throw new Error('Title, imgURL, and videoURL are required.');
+  }
+
+  try {
+    await sql(
+      `INSERT INTO uploads (title, subtitle, description, imgURL, videoURL) 
+       VALUES ($1, $2, $3, $4, $5)`,
+      [title, subtitle, description, imgURL, videoURL]
+    );
+    console.log('Data inserted successfully!');
+  } catch (error) {
+    console.error('Error inserting data:', error);
+    throw new Error('Database insertion failed.');
+  }
+};
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -29,9 +29,7 @@ const page = () => {
         </ul>
       </div>
 
-      <div className='md:hidden flex py-10 h-[30rem] w-full md:h-full items-center justify-center bg-cover bg-no-repeat bg-[url("https://images.squarespace-cdn.com/content/v1/668baa90a93f73799f48b8ba/1726027163743-QYWYQZGF887V6VLJ475T/unsplash-image-iBEPt9udRz4.jpg?format=2500w")]'>
-        <h1 className='text-4xl md:text-7xl text-white font-bold'>FILMING</h1>
-      </div>
+      
       <div className='py-10 flex items-center justify-center gap-4 min-h-[40rem] text-3xl text-white'>
         <ul className='flex flex-col gap-10 text-center md:text-right' >
           <li>Full Production & Filming Crew <br/> i.e. Producer, Director, DoP, Sound, Drone, PA, etc.</li>
@@ -39,12 +37,12 @@ const page = () => {
           <li>Logistical Support & Coordination</li>
         </ul>
       </div>
-      <div className='md:block hidden py-10 w-full h-full items-center justify-center bg-cover bg-no-repeat bg-[url("https://images.squarespace-cdn.com/content/v1/668baa90a93f73799f48b8ba/1726027163743-QYWYQZGF887V6VLJ475T/unsplash-image-iBEPt9udRz4.jpg?format=2500w")]'>
+      <div className=' flex py-10 w-full h-full items-center justify-center bg-cover bg-no-repeat bg-[url("https://images.squarespace-cdn.com/content/v1/668baa90a93f73799f48b8ba/1726027163743-QYWYQZGF887V6VLJ475T/unsplash-image-iBEPt9udRz4.jpg?format=2500w")]'>
         <h1 className='text-4xl md:text-7xl text-white font-bold'>FILMING</h1>
       </div>
 
-      <div className='md:hidden flex py-10 h-[30rem] w-full md:h-full items-center justify-center bg-cover bg-no-repeat bg-[url("https://images.squarespace-cdn.com/content/v1/668baa90a93f73799f48b8ba/1726027199352-EYD3D2QN7HWL879V5CO6/unsplash-image-U4-_lvnjkd8.jpg?format=2500w")]'>
-        <h1 className='text-4xl md:text-7xl text-white font-bold'>FILMING</h1>
+      <div className=' flex py-10 h-[30rem] w-full md:h-full items-center justify-center bg-cover bg-no-repeat bg-[url("https://images.squarespace-cdn.com/content/v1/668baa90a93f73799f48b8ba/1726027199352-EYD3D2QN7HWL879V5CO6/unsplash-image-U4-_lvnjkd8.jpg?format=2500w")]'>
+        <h1 className='text-4xl md:text-7xl text-white font-bold'>POSTPRODUCTION</h1>
       </div>
       <div className='py-10 flex items-center justify-center md:pl-10  gap-4 min-h-[40rem] text-3xl  text-white'>
           <ul className='flex flex-col gap-4 text-center md:text-left' >
