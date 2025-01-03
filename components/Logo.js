50b824114665f00a1737f1b21244d0b47a9e140e
Jonlaarsen <jontoftdallarsen@icloud.com>
2025-01-03 19:03:55 +0900

    test2-update

--- components/Logo.js
+++ components/Logo.js
@@ -1,31 +1,31 @@
-"use client"
+// "use client"
 
-import { Canvas } from "@react-three/fiber"
-import { Suspense } from "react"
-import { Center, useAnimations, useGLTF } from "@react-three/drei"
+// import { Canvas } from "@react-three/fiber"
+// import { Suspense } from "react"
+// import { Center, useAnimations, useGLTF } from "@react-three/drei"
 
-useGLTF('/postedM.gltf')
+// useGLTF('/postedM.gltf')
 
-function Logo(){
-    const {nodes, animations, scene} = useGLTF('/postedM.gltf')
-    const {actions} = useAnimations(animations, scene)
-    return(
-        <group>
-            <primitive 
-            object={scene}
-            />
-        </group>    
-    )
-}
+// function Logo(){
+//     const {nodes, animations, scene} = useGLTF('/postedM.gltf')
+//     const {actions} = useAnimations(animations, scene)
+//     return(
+//         <group>
+//             <primitive 
+//             object={scene}
+//             />
+//         </group>    
+//     )
+// }
 
-export default function Scene(){
-    return(
-    <Canvas gl={{antialias: true}} dpr={[1,1.5]}>
-        <directionalLight position={[-5,-5,5]} intensity={2} />
-        <Suspense fallback={null}/>
-        <Center>
-            <Logo/>
-        </Center>
-    </Canvas>
-   )
-}
+// export default function Scene(){
+//     return(
+//     <Canvas gl={{antialias: true}} dpr={[1,1.5]}>
+//         <directionalLight position={[-5,-5,5]} intensity={2} />
+//         <Suspense fallback={null}/>
+//         <Center>
+//             <Logo/>
+//         </Center>
+//     </Canvas>
+//    )
+// }
