82394605b6801647d763980ee8b404d8e5f111a3
Jonlaarsen <jontoftdallarsen@icloud.com>
2025-01-20 16:08:48 +0900

    heroupdate

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -34,22 +34,22 @@ const Hero = () => {
         ></iframe>
          
         </div> */}
-        <div
-          className="flex top-0 left-0 h-[100vh] w-auto md:w-full md:h-screen items-center justify-center"
-          
-        >
-          <iframe
-            rel="preload"
-            src="https://geo.dailymotion.com/player.html?video=x98a970"            
-            playsInline
-            autoPlay
-            loop
-            className="w-screen h-screen object-cover"
+        {/* <div className="flex top-0 left-0  h-screen md:h-[56vw] max-w-screen w-full items-center justify-center relative overflow-hidden">
+          <iframe 
+          src="https://player.vimeo.com/video/1048469458?h=3224553ac2&amp;autoplay=1&amp;controls=0&amp;loop=1&amp;muted=1" 
+          allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+          className="w-full h-screen md:h-full object-cover"
           ></iframe>
-          
+        </div> */}
+        <div className="flex top-0 left-0 h-screen w-full items-center justify-center overflow-hidden relative">
+          <iframe 
+            src="https://player.vimeo.com/video/1048469458?h=3224553ac2&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1&amp;controls=0&amp;loop=1"
+            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+            className="absolute top-1/2 left-1/2 w-[177.77777778vh] h-[56.25vw] min-h-full min-w-full transform -translate-x-1/2 -translate-y-1/2 box-border pointer-events-none"
+          ></iframe>
         </div>
-        {/* <RotatingLogo/> */}
-        
+
+        {/* <script src="https://player.vimeo.com/api/player.js"></script> */}
 
       </div>
     </div>
