b964fab6abdb0f97ca133284ab6988ebbf807baa
Jonlaarsen <jontoftdallarsen@icloud.com>
2025-02-04 21:20:45 +0900

    contactfi

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -13,8 +13,8 @@ const page = () => {
      className='grid grid-cols-1 md:grid-cols-2 pt-[12rem] pb-10  gap-4'>
       <div >
         <div className='md:pl-5 '>
-          <h1 className=' text-5xl md:text-8xl px-4 font-bold  md:pb-[5rem]'>CONTACT US</h1>
-          <p className=' text-xl font-[300] px-5 w-[90%] pb-[3rem]'>Interested in working with us? <br/> Fill out some info and we will be in touch shortly!</p>
+          <h1 className=' text-5xl md:text-8xl px-4 font-bold  md:pb-[2rem]'>CONTACT US</h1>
+          <p className=' text-xl font-[300] px-5 w-[90%] pb-[1rem]'>Interested in working with us? <br/> Fill out some info and we will be in touch shortly!</p>
         </div>
         
         <form 
@@ -43,7 +43,7 @@ const page = () => {
           <div className='flex px-4 md:px-0 flex-col w-[99vw] md:w-[80%]'>
             <label htmlFor="email">Message</label>
             <textarea 
-            className='h-[5rem] md:pl-5  text-black'
+            className='h-[4rem] md:pl-5 md:pt-2  text-black'
             placeholder='required' name="message" type='text' required></textarea>
           </div>
           <div className='px-4 md:px-0 w-[99vw] md:w-[80%]'>
@@ -52,10 +52,10 @@ const page = () => {
         </form>
       </div>
       <div className='w-full flex justify-center items-center'>
-        <div className='w-full  overflow-hidden'>
+        <div className='w-full h-[41rem]    overflow-hidden'>
           <iframe src="https://player.vimeo.com/video/1051779255?h=e8c1e1223f&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;background=1" 
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" 
-          className=" w-[65rem] h-screen object-fill hidden md:block "        title="Contact us Video">
+          className=" w-[75rem] h-[65rem] mt-[-10rem]  object-fill hidden md:flex "        title="Contact us Video">
           </iframe></div>
         <script src="https://player.vimeo.com/api/player.js"></script>
       </div>
