7ae9b82caf8065a79ec623f2327a9294e3b7874d
Jonlaarsen <jontoftdallarsen@icloud.com>
2025-02-04 16:37:24 +0900

    vertical video fix aws s3

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="md:min-h-screen z-10">
+    <div className="md:min-h-screen ">
       <div className="flex flex-col items-center gap-8 justify-center">
         {/* Background video for larger screens */}
         <div className="top-0 left-0 h-screen w-full items-center justify-center overflow-hidden relative md:block hidden">
@@ -16,14 +16,19 @@ const Hero = () => {
         </div>
 
         {/* Vertical video for mobile screens */}
-        <div className=" top-0 left-0 h-screen w-full items-center justify-center overflow-hidden relative md:hidden block">
-          <iframe
-            src="https://player.vimeo.com/video/1050681038?h=746df36bf2&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1&amp;controls=0&amp;loop=1&amp;dnt=1"
-            frameBorder="0"
-            allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
-            title="Poster Vertical"
-            className="absolute top-0 left-0 w-screen h-screen box-border"
-          ></iframe>
+        <div 
+          className="flex top-0 left-0 h-[100vh] w-auto md:w-screen md:h-screen items-center justify-center" >
+          <video
+          src="https://videos-clone-local-tommy.s3.ap-northeast-2.amazonaws.com/Poster+Vertical.mp4"
+            // src="https://player.vimeo.com/video/1050681038?h=746df36bf2&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1&amp;controls=0&amp;loop=1&amp;dnt=1"
+            autoPlay
+            playsInline
+            loop
+            muted
+            // allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+            // title="Poster Vertical"
+            className="w-screen h-screen object-cover"
+          ></video>
         </div>
       </div>
       <script src="https://player.vimeo.com/api/player.js"></script>
