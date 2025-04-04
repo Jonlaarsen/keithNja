f7f04a6bbeb90501c921448052d9dda0c469d991
Jonlaarsen <jontoftdallarsen@icloud.com>
2025-04-04 12:15:52 +0200

    test

--- app/api/sitemap.js
+++ app/api/sitemap.js
@@ -1,6 +1,5 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/xml");
-  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
           <loc>https://www.posted-productions.com/</loc>
@@ -8,5 +7,11 @@ export default function handler(req, res) {
           <changefreq>daily</changefreq>
           <priority>1.0</priority>
       </url>
-  </urlset>`);
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
